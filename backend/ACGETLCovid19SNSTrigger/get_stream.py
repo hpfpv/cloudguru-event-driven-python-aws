402bfffb7b7e14a293e106f44d127230461ad755
H. Pierre-Francois <81161636+hpfpv@users.noreply.github.com>
2021-05-29 22:26:56 +0100

    Add files via upload

---
+++ backend/ACGETLCovid19SNSTrigger/get_stream.py
@@ -0,0 +1,22 @@
+import json
+import boto3
+
+def get_stream_insert(event, context):
+    try:
+        new_rows_count = 0
+        for record in event ['Records']:
+            if record['eventName'] == 'INSERT':
+                new_rows_count += 1
+        return new_rows_count
+    except Exception as er:
+        print(er)
+
+def get_stream_remove(event, context):
+    try:
+        del_rows_count = 0
+        for record in event ['Records']:
+            if record['eventName'] == 'REMOVE':
+                del_rows_count += 1
+        return del_rows_count
+    except Exception as er:
+        print(er)
---
+++ backend/ACGETLCovid19SNSTrigger/trigger_sns.py
@@ -0,0 +1,22 @@
+import boto3
+from get_stream import *
+
+def main(event, context):
+    try:
+        sns = boto3.resource('sns')
+        topic = sns.Topic('arn:aws:sns:us-east-1:601091111123:ETLCovid19Topic')
+        newItems = str(get_stream_insert(event, context))
+        delItems = str(get_stream_remove(event, context))
+        response = topic.publish(
+            Message = 'The ETL job is completed. The table has ' + newItems + ' new items and ' + delItems + ' deleted items.' ,
+            Subject = 'ETLCovid19 job status',
+            MessageStructure = 'string'
+        )
+        print(str(response) + ' has been published.')
+        return response
+    except Exception as er:
+        print(er)
+        print('Couldnt publish message to SNS')
+    
+if __name__ == '__main__':
+    main(event, context)
